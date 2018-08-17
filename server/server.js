1fef2bc95e103b33f0042aef5e35dd9ff1e5737b
Mike <you@example.com>
2018-08-16 18:28:43 -0700

    Add tests for GET /todos/:id

--- server/server.js
+++ server/server.js
@@ -42,11 +42,11 @@ app.get('/todos/:id', (req, res) => {
     }
     Todo.findById(id).then((todo) => {
         if (!todo) {
-            return res.status(400).send();
+            return res.status(404).send();
         }
         res.send({todo});
     }).catch((e) => {
-        res.status(400).send();
+        res.status(404).send();
     });
 });
 
