8bb3b2d055a2d8c02c616972d15fbaf195fff951
Mike <you@example.com>
2018-08-17 13:45:05 -0700

    add get /users/me

--- server/server.js
+++ server/server.js
@@ -8,6 +8,7 @@ const {ObjectID} = require('mongodb');
 var mongoose = require('./db/mongoose');
 var {Todo} = require('./models/todo');
 var {User} = require('./models/user');
+var {authenticate} = require('./middleware/authenticate');
 
 var app = express();
 const port = process.env.PORT;
@@ -100,8 +101,7 @@ app.patch('/todos/:id', (req, res) => {
 });
 
 // Users
-var usersUrl = '/users';
-app.post(usersUrl, (req, res) => {
+app.post('/users', (req, res) => {
     var body = _.pick(req.body, ['email','password']);
     var user = new User(body);
 
@@ -113,6 +113,10 @@ app.post(usersUrl, (req, res) => {
     }).catch((e) => {
         res.status(400).send(e);
     })
+});
+
+app.get('/users/me', authenticate, (req, res) => {
+    res.send(req.user);
 });
 
 app.listen(port, ()  => {
