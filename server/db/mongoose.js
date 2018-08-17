06f3dceefb04ce48ca48b056f5dfef02935172e5
Mike <you@example.com>
2018-08-17 10:41:42 -0700

    setup different test and devel

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,7 +1,7 @@
 var mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
-const uri = process.env.MONGODB_URI || 'mongodb://localhost:27017/TodoApp';
+const uri = process.env.MONGODB_URI;
 mongoose.connect(uri,{ useNewUrlParser: true });
 
 module.exports = {mongoose};
